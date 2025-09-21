e13f9679f2749b2f7a68248a22221b81364f951e
Dika Afty <dikaafty@gmail.com>
2025-09-21 22:42:15 +0700

    Create animation using gsap.fromTo() method

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,5 +1,25 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFromTo = () => {
   // TODO: Implement the gsap.fromTo() method
+  useGSAP(() => {
+    gsap.fromTo("#red-box", 
+      {
+        x: 0,
+        rotate: 0,
+        borderRadius: 0,
+      },
+      {
+        x: 300,
+        duration: 1.5,
+        rotate: 180,
+        borderRadius: 100,
+        repeat: -1,
+        yoyo: true,
+        ease: "circ.inOut",
+      });
+  }, []);
 
   return (
     <main>
