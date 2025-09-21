1aa28a6420e204ca79681b0f43c7272781e718a8
Dika Afty <dikaafty@gmail.com>
2025-09-22 01:55:08 +0700

    Create scroll trigger animation

--- src/pages/GsapScrollTrigger.jsx
+++ src/pages/GsapScrollTrigger.jsx
@@ -1,5 +1,32 @@
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const GsapScrollTrigger = () => {
+  const scrollRef = useRef();
+
   // TODO: Implement the gsap scroll trigger
+  useGSAP(() => {
+    const boxes = gsap.utils.toArray(scrollRef.current.children);
+
+    boxes.forEach(box => {
+      gsap.to(box, {
+        x: 400,
+        borderRadius: 100,
+        scale: 2,
+        scrollTrigger: {
+          trigger: box,
+          start: "top bottom",
+          end: "top 30%",
+          scrub: true, // make the animation smooth
+        },
+        ease: "power1.inOut"
+      });
+    });
+  }, { scope: scrollRef });
 
   return (
     <main>
@@ -51,7 +78,10 @@ const GsapScrollTrigger = () => {
         </svg>
       </div>
 
-      <div className="mt-20 w-full h-screen">
+      <div 
+        className="mt-20 w-full h-screen"
+        ref={scrollRef}  
+      >
         <div
           id="scroll-pink"
           className="scroll-box w-20 h-20 rounded-lg bg-pink-500"
