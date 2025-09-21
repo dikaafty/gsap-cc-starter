2460435a42f8c324c0a2560be6c350f957140aee
Dika Afty <dikaafty@gmail.com>
2025-09-21 22:33:20 +0700

    Create animation using gsap.from() method

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 400,
+      duration: 1.5,
+      rotate: 180,
+      repeat: -1,
+      yoyo: true,
+      ease: "expo.inOut"
+    });
+  }, []);
 
   return (
     <main>
